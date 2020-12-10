6335578b85e627740eb5f4cf3f24ce28c23483fb
Jeong Unsan <74296888+Action2theFuture@users.noreply.github.com>
2020-12-10 19:43:19 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -8,7 +8,7 @@ function handleImgLoad(){
 
 function paintImage(imgNumber){
   const image = new Image();
-  image.src = `images/${imgNumber + 1}.jpg`;
+  image.src = `/${imgNumber + 1}.jpg`;
   image.classList.add("bgImage");
   body.prepend(image);
 }
