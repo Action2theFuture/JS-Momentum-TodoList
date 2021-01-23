f595bdd1896438fc55e72e6dfbdd5677c736c89f
Jeong Unsan <74296888+Action2theFuture@users.noreply.github.com>
2021-01-23 18:48:54 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -2,24 +2,20 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 3;
 
-function handleImgLoad(){
-
-}
-
-function paintImage(imgNumber){
+function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `root/${imgNumber + 1}.jpg`;
+  image.src = `${imgNumber + 1}.jpg`;
   image.classList.add("bgImage");
-  body.prepend(image);
+  body.appendChild(image);
 }
 
-function genRandom(){
+function genRandom() {
   const number = Math.floor(Math.random() * IMG_NUMBER);
   return number;
 }
 
-function init(){
-  const randomNumber = genRandom(); 
+function init() {
+  const randomNumber = genRandom();
   paintImage(randomNumber);
 }
 
